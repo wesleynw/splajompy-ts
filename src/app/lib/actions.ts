c4a073c4c6bec2095eb5d166f26e362556e3caf6
Wesley Weisenberger <wesleynw@pm.me>
2024-10-15 22:49:11 -0400

    improvements

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -4,8 +4,8 @@ import { signIn } from "@/auth";
 import bcrypt from "bcryptjs";
 import { registerSchema } from "./zod";
 import { sql } from "@vercel/postgres";
-import { redirect } from "next/navigation";
 import { CredentialsSignin } from "next-auth";
+import zod from "zod";
 
 export async function authenticate(_currentState: unknown, formData: FormData) {
   try {
@@ -26,27 +26,21 @@ export async function register(_currentState: unknown, formData: FormData) {
   const email = formData.get("email")?.toString() || "";
   const password = formData.get("password")?.toString() || "";
 
-  console.log("registering with password: ", password);
-
-  // Validate input using Zod
-  const parsedData = registerSchema.parse({ email, password });
-
-  // Hash the password
-  const hashedPassword = await bcrypt.hash(parsedData.password, 10);
-
-  console.log("hashed: ", hashedPassword);
-
-  // Insert user into Vercel Postgres
   try {
-    await sql`
-    INSERT INTO users (email, password)
-    VALUES (${parsedData.email}, ${hashedPassword});
-  `;
-  } catch (error) {
-    console.error(error);
-  }
+    const parsedData = registerSchema.parse({ email, password });
 
-  // Redirect to a success page or login
-  redirect("/login");
-  return "lol";
+    const hashedPassword = await bcrypt.hash(parsedData.password, 10);
+
+    await sql`
+      INSERT INTO users (email, password)
+      VALUES (${parsedData.email}, ${hashedPassword});
+    `;
+  } catch (error) {
+    if (error instanceof zod.ZodError) {
+      return error.errors.map((e) => e.message).join(", ");
+    }
+
+    console.error("Registration error:", error);
+    return "An error occurred while registering. Please try again.";
+  }
 }
