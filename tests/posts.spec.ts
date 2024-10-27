15f86b44b46e3b837aed4e7da1ee0212b2a9334f
Wesley Weisenberger <wesleynw@pm.me>
2024-10-26 21:50:04 -0400

    more tests

--- tests/posts.spec.ts
+++ tests/posts.spec.ts
@@ -4,14 +4,16 @@ import { createClient } from "@vercel/postgres";
 test("create a post", async ({ page }) => {
   // TODO: paramaterize account creation
   // delete test user from previous runs
-  const client = createClient({ connectionString: process.env.POSTGRES_URL });
+  const client = createClient({
+    connectionString: process.env.POSTGRES_URL_NON_POOLING,
+  });
   await client.connect();
-  await client.sql`DELETE FROM users WHERE username = 'developers+test1'`;
+  await client.sql`DELETE FROM users WHERE username = 'developers+a+test1'`;
 
   // create an account
   await page.goto("/register");
-  await page.fill('input[name="username"]', "developers+test1");
-  await page.fill('input[name="email"]', "developers+test1@splajompy.com");
+  await page.fill('input[name="username"]', "developers+a+test1");
+  await page.fill('input[name="email"]', "developers+a+test1@splajompy.com");
   await page.fill('input[name="password"]', "b7NBsmzkiKnFumaMFifz$");
   await page.click('button[type="submit"]');
 
@@ -29,5 +31,44 @@ test("create a post", async ({ page }) => {
   );
 
   // cleanup
-  await client.sql`DELETE FROM users WHERE username = 'developers+test1'`; // also deletes the post
+  await client.sql`DELETE FROM users WHERE username = 'developers+a+test1'`; // also deletes the post
+});
+
+test("create a post and make sure other users can see it", async ({ page }) => {
+  // delete test user from previous runs
+  const client = createClient({
+    connectionString: process.env.POSTGRES_URL_NON_POOLING,
+  });
+  await client.connect();
+  await client.sql`DELETE FROM users WHERE username = 'developers+a+test2'`;
+
+  // create an account
+  await page.goto("/register");
+  await page.fill('input[name="username"]', "developers+a+test2");
+  await page.fill('input[name="email"]', "developers+a+test2@splajompy.com");
+  await page.fill('input[name="password"]', "b7NBsmzkiKnFumaMFifz$");
+  await page.click('button[type="submit"]');
+
+  // post
+  await page.fill('input[name="text"]', "My second post");
+  await page.click('button[type="submit"]');
+  await page.waitForSelector("li:has-text('My second post')");
+  await page.click('button:has-text("Sign Out")');
+
+  // create another account
+  await page.goto("/register");
+  await page.fill('input[name="username"]', "developers+a+test3");
+  await page.fill('input[name="email"]', "developers+a+test3@splajompy.com");
+  await page.fill('input[name="password"]', "b7NBsmzkiKnFumaMFifz$");
+  await page.click('button[type="submit"]');
+
+  // check if the post is visible
+  await page.waitForSelector("li:has-text('My second post')");
+  await expect(page.locator("li:has-text('My second post')")).toContainText(
+    "My second post"
+  );
+
+  // cleanup
+  await client.sql`DELETE FROM users WHERE username = 'developers+a+test2'`;
+  await client.sql`DELETE FROM users WHERE username = 'developers+a+test3'`;
 });
