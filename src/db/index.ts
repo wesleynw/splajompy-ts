3b97a794fb81b7ba9db08a8d92ec54291999150c
Wesley Weisenberger <wesleynw@pm.me>
2024-11-12 17:47:02 -0500

    fix new DO db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle({ client: sql });
+export const db = drizzle(process.env.POSTGRES_URL!);
