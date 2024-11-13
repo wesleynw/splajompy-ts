40e103383f731d80b64e2df7eda78e18eeaa7ae8
Wesley Weisenberger <wesleynw@pm.me>
2024-11-13 17:02:49 -0500

    move cert to env

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,16 +1,12 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import fs from "fs";
-import path from "path";
-
-const certPath = path.join(process.cwd(), "src/db/ca-certificate.crt");
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
   ssl: {
     rejectUnauthorized: true,
-    ca: fs.readFileSync(certPath).toString(),
+    ca: process.env.CA_CERT!,
   },
 });
 
