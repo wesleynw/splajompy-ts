de951da15c11fae3a8b6273c07d7bf6ec17980c3
Wesley Weisenberger <wesleynw@pm.me>
2024-12-08 11:40:35 -0500

    fix profile page formatting

--- src/app/components/user/UserView.tsx
+++ src/app/components/user/UserView.tsx
@@ -30,75 +30,79 @@ export default function UserView({ user }: Readonly<Props>) {
   const isOwnProfile = session?.user?.user_id === user.user_id;
 
   return (
-    <Box sx={{ px: { xs: 2, md: 4 } }}>
-      <Box
-        sx={{
-          maxWidth: 600,
-          borderRadius: "8px",
-          padding: 3,
-          background: "linear-gradient(135deg, #ffffff, #f9f9f9)",
-          boxShadow: "0 2px 8px rgba(0, 0, 0, 0.1)",
-          margin: "10px auto",
-          ...theme.applyStyles("dark", {
-            background: "linear-gradient(135deg, #1b1b1b, #2a2a2a)",
-            boxShadow: "0 2px 8px rgba(0, 0, 0, 0.5)",
-          }),
-        }}
-      >
-        <Box display="flex" justifyContent="space-between">
-          <BackButton />
-          {isOwnProfile && (
-            <Button
-              variant="contained"
-              size="medium"
-              onClick={() => {
-                signOut({ redirectTo: "/login" });
-              }}
-              sx={{
-                textTransform: "none",
-                borderRadius: "20px",
-                paddingX: 2,
-                paddingY: 0.5,
-                fontWeight: "bold",
-                fontSize: "0.875rem",
-                minWidth: "auto",
-                backgroundColor: "#1DA1F2",
-                color: "#ffffff",
-                ...theme.applyStyles("dark", {
+    <Box>
+      <Box sx={{ px: { xs: 2, md: 4 } }}>
+        <Box
+          sx={{
+            width: "100%",
+            maxWidth: 600,
+            borderRadius: "8px",
+            gap: 1,
+            padding: 3,
+            background: "linear-gradient(135deg, #ffffff, #f9f9f9)",
+            boxShadow: "0 2px 8px rgba(0, 0, 0, 0.1)",
+            margin: "10px auto",
+            ...theme.applyStyles("dark", {
+              background: "linear-gradient(135deg, #1b1b1b, #2a2a2a)",
+              boxShadow: "0 2px 8px rgba(0, 0, 0, 0.5)",
+            }),
+          }}
+        >
+          <Box display="flex" justifyContent="space-between">
+            <BackButton />
+            {isOwnProfile && (
+              <Button
+                variant="contained"
+                size="medium"
+                onClick={() => {
+                  signOut({ redirectTo: "/login" });
+                }}
+                sx={{
+                  textTransform: "none",
+                  borderRadius: "20px",
+                  paddingX: 2,
+                  paddingY: 0.5,
+                  fontWeight: "bold",
+                  fontSize: "0.875rem",
+                  minWidth: "auto",
                   backgroundColor: "#1DA1F2",
-                }),
-                "&:hover": {
-                  backgroundColor: "#0d8de6",
-                },
+                  color: "#ffffff",
+                  ...theme.applyStyles("dark", {
+                    backgroundColor: "#1DA1F2",
+                  }),
+                  "&:hover": {
+                    backgroundColor: "#0d8de6",
+                  },
+                }}
+              >
+                Sign Out
+              </Button>
+            )}
+            <FollowButton user_id={user.user_id} show_unfollow={true} />
+          </Box>
+          <Stack
+            direction="row"
+            alignItems="left"
+            justifyContent="space-between"
+            marginTop={1}
+          >
+            <Typography
+              variant="h6"
+              sx={{
+                fontWeight: "bold",
+                color: "#333333",
+                wordBreak: "break-all",
+                textAlign: "center",
+                marginLeft: 1,
+                ...theme.applyStyles("dark", { color: "#ffffff" }),
               }}
             >
-              Sign Out
-            </Button>
-          )}
-          <FollowButton user_id={user.user_id} show_unfollow={true} />
+              @{user.username}
+            </Typography>
+          </Stack>
         </Box>
-        <Stack
-          direction="row"
-          alignItems="left"
-          justifyContent="space-between"
-          marginTop={1}
-        >
-          <Typography
-            variant="h6"
-            sx={{
-              fontWeight: "bold",
-              color: "#333333",
-              wordBreak: "break-all",
-              textAlign: "center",
-              marginLeft: 1,
-              ...theme.applyStyles("dark", { color: "#ffffff" }),
-            }}
-          >
-            @{user.username}
-          </Typography>
-        </Stack>
       </Box>
-      <Box sx={{}}>
+      <Box>
         <Feed
           session={session}
           feedType="profile"
