c720ba1cf47a9d7c275e7961a69f0a84ba0403eb
Wesley Weisenberger <wesleynw@pm.me>
2024-12-11 22:06:12 -0500

    standalone mode 2

--- src/app/components/navigation/MobileNavigation.tsx
+++ src/app/components/navigation/MobileNavigation.tsx
@@ -14,7 +14,10 @@ export default function MobileNavigation() {
   const router = useRouter();
   const { data: session } = useSession();
 
-  const isStandalone = typeof window === "undefined";
+  const is_standalone =
+    typeof window !== "undefined" &&
+    (window.matchMedia("(display-mode: standalone)").matches ||
+      window.navigator.standalone === true);
 
   const handleNavigation = (event: React.MouseEvent, targetPath: string) => {
     if (targetPath === pathname) {
@@ -39,7 +42,8 @@ export default function MobileNavigation() {
       <BottomNavigation
         value={pathname}
         sx={{
-          height: isStandalone ? "72px" : "56px", // Custom height for standalone mode
+          height: is_standalone ? "80px" : "56px", // Custom height for standalone mode
+          alignItems: "flex-start", // Align items closer to the top
         }}
       >
         <BottomNavigationAction
@@ -47,6 +51,9 @@ export default function MobileNavigation() {
           icon={<HomeIcon />}
           onClick={(event) => handleNavigation(event, "/")}
           disableRipple
+          sx={{
+            paddingTop: "15px",
+          }}
         />
         <BottomNavigationAction
           value="/notifications"
@@ -57,12 +64,18 @@ export default function MobileNavigation() {
           }
           onClick={(event) => handleNavigation(event, "/notifications")}
           disableRipple
+          sx={{
+            paddingTop: "15px",
+          }}
         />
         <BottomNavigationAction
           value="/all"
           icon={<PublicIcon />}
           onClick={(event) => handleNavigation(event, "/all")}
           disableRipple
+          sx={{
+            paddingTop: "15px",
+          }}
         />
         <BottomNavigationAction
           value={`/user/${session?.user?.username}`}
@@ -71,6 +84,9 @@ export default function MobileNavigation() {
             handleNavigation(event, `/user/${session?.user?.username}`)
           }
           disableRipple
+          sx={{
+            paddingTop: "15px",
+          }}
         />
       </BottomNavigation>
     </Box>
