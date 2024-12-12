0db94cdb47ecbb9bbe4d9c14264f20a173d641de
Wesley Weisenberger <wesleynw@pm.me>
2024-12-11 21:41:26 -0500

    prefetch

--- src/app/components/navigation/Navigation.tsx
+++ src/app/components/navigation/Navigation.tsx
@@ -3,10 +3,22 @@
 import { NoSsr, useMediaQuery, useTheme } from "@mui/material";
 import MobileNavigation from "./MobileNavigation";
 import DesktopNavigation from "./DesktopNavigation";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export default function Navigation() {
+  const router = useRouter();
   const theme = useTheme();
+  const { data: session } = useSession();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"), { noSsr: true });
+
+  useEffect(() => {
+    router.prefetch("/");
+    router.prefetch("/notifications");
+    router.prefetch("/all");
+    router.prefetch(`/user/${session?.user?.username}`);
+  }, [router, session]);
 
   return (
     <NoSsr>{isMobile ? <MobileNavigation /> : <DesktopNavigation />}</NoSsr>
