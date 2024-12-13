049e576a9e4fe889b23c80689977bd8a4560ee05
Wesley Weisenberger <wesleynw@pm.me>
2024-12-12 19:23:16 -0500

    try server component again

--- src/app/(authenticated)/all/page.tsx
+++ src/app/(authenticated)/all/page.tsx
@@ -1,12 +1,18 @@
-"use client";
-
 import Feed from "@/app/components/feed/Feed";
 import { Box } from "@mui/material";
 import { Suspense } from "react";
 import FeedSkeleton from "@/app/components/loading/FeedSkeleton";
 import StandardWrapper from "@/app/components/loading/StandardWrapper";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return (
     <Box
       sx={{
