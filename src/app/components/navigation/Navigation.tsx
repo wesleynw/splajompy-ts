725b976a767d6fc844f21c12777c488a4c9bbed1
Wesley Weisenberger <wesleynw@pm.me>
2025-01-22 20:35:37 -0500

    remove prefetch

--- src/app/components/navigation/Navigation.tsx
+++ src/app/components/navigation/Navigation.tsx
@@ -3,22 +3,10 @@
 import { NoSsr, useMediaQuery } from "@mui/material";
 import MobileNavigation from "./MobileNavigation";
 import DesktopNavigation from "./DesktopNavigation";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 import theme from "@/theme";
-import { useUser } from "@/app/providers/UserProvider";
 
 export default function Navigation() {
-  const router = useRouter();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"), { noSsr: true });
-  const user = useUser();
-
-  useEffect(() => {
-    router.prefetch("/");
-    router.prefetch("/notifications");
-    router.prefetch("/all");
-    router.prefetch(`/user/${user.username}`);
-  }, [router]);
 
   return (
     <NoSsr>{isMobile ? <MobileNavigation /> : <DesktopNavigation />}</NoSsr>
