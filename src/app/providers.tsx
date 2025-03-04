a80b126d337473aa284c819f3de1230f9a0740e0
Wesley Weisenberger <wesleynw@pm.me>
2025-03-04 17:51:36 -0500

    reverse proxy

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -12,8 +12,8 @@ export function PostHogProvider({
 }: Readonly<{ children: React.ReactNode }>) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host:
-        process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com",
+      api_host: "/ingest",
+      ui_host: "https://us.posthog.com",
       person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
       capture_pageview: false, // Disable automatic pageview capture, as we capture manually
     });
