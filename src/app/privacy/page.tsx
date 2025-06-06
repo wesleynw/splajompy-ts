8df28eca3d98cd5ee8728b1a254dd88cec12f069
Wesley Weisenberger <wesleynw@pm.me>
2025-06-06 14:52:46 -0400

    privacy policy, package updates

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,21 @@
+export const metadata = {
+  title: "Privacy Policy",
+};
+
+export default function Privacy() {
+  return (
+    <main className="mx-auto my-8 max-w-xl rounded-lg p-8 font-sans shadow-md">
+      <h1 className="mb-4 text-2xl font-bold">Privacy Policy</h1>
+      <p className="mb-4">
+        Splajompy respects your privacy. We do not share or sell any personal
+        information. Your use of this site is secure, and stored login data is
+        private and encrypted. Minimal usage data is collected to monitor and
+        improve the performance of the site.
+      </p>
+      <p>
+        If you have any questions about our privacy policy, please email{" "}
+        <a>wesleynw [at] pm.me</a>.
+      </p>
+    </main>
+  );
+}
