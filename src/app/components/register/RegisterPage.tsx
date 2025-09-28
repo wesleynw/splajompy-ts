e30da6df40c1af9428517bfc33d0f3725db2666f
Wesley Weisenberger <wesleynw@pm.me>
2025-09-28 11:09:58 -0400

    disable register

--- src/app/components/register/RegisterPage.tsx
+++ src/app/components/register/RegisterPage.tsx
@@ -1,33 +1,59 @@
 "use client";
 
-import { register } from "@/app/auth/register";
+// import { register } from "@/app/auth/register";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import React, { useActionState } from "react";
-import { useFormStatus } from "react-dom";
+import React from "react";
+// import { useFormStatus } from "react-dom";
 import logo from "../../apple-icon.png";
-import Form from "../base/form/Form";
-import Input from "../base/form/Input";
-import Button from "../button/Button";
+// import Form from "../base/form/Form";
+// import Input from "../base/form/Input";
+// import Button from "../button/Button";
 import CenteredLayout from "../layout/CenteredLayout";
 
-const initialState = {
-  errors: {
-    username: "",
-    email: "",
-    password: "",
-  },
-  payload: undefined,
-};
+// const initialState = {
+//   errors: {
+//     username: "",
+//     email: "",
+//     password: "",
+//   },
+//   payload: undefined,
+// };
 
 export default function RegisterPage() {
   const router = useRouter();
-  const { pending } = useFormStatus();
-  const [state, dispatch] = useActionState(register, initialState);
+  // const { pending } = useFormStatus();
+  // const [state, dispatch] = useActionState(register, initialState);
 
   return (
     <div className="mt-16 flex w-screen flex-col justify-center">
       <CenteredLayout>
+        <div className="flex flex-col items-center">
+          <Image
+            width={100}
+            height={100}
+            src={logo}
+            alt="Logo"
+            className="mb-7 rounded-3xl shadow-[0_0_15px_5px_rgba(255,255,255,0.3)]"
+          />
+          <p className="mb-5 text-2xl font-black">Register</p>
+          <div className="mb-8 max-w-md text-center">
+            <p className="mb-4 text-lg">
+              Registration is now only available through the iOS app.
+            </p>
+          </div>
+          <div className="mt-2 flex flex-row space-x-2.5">
+            <p className="font-bold">Already have an account?</p>
+            <button
+              className="underline"
+              onClick={() => router.push("/login")}
+              type="button"
+            >
+              <p className="font-black">Login</p>
+            </button>
+          </div>
+        </div>
+        {/*
         <Form action={dispatch}>
           <Image
             width={100}
@@ -81,6 +107,7 @@ export default function RegisterPage() {
             </button>
           </div>
         </Form>
+        */}
       </CenteredLayout>
     </div>
   );
