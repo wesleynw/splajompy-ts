e26cd888191b58eef9b88ef4c655af321cd9732b
Wesley Weisenberger <wesleynw@pm.me>
2026-03-08 23:12:22 -0400

    private

--- src/app/lib/s3.ts
+++ src/app/lib/s3.ts
@@ -36,7 +36,7 @@ export async function getPresignedUrl(
       Bucket: process.env.SPACE_NAME!,
       Key: `${uniqueFilename}`,
       Fields: {
-        acl: "public-read",
+        acl: "private",
         ContentType: contentType,
       },
     });
